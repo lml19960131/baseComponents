5a071c430c5bb4492b6eb6f47015a12f4ae95e84
lml19960131 <lml19960131@126.com>
2018-02-17 21:22:43 +0800

    confirm-test

---
+++ src/test/confirm/confirm.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <p>{{text}}</p>
+    <button @click="showConfirm">全部删除</button>
+    <confirm text="是否全部删除" sureBtn="确定" cancelBtn="取消" :showFlag="false"
+             @sure="confirmClear" @cancel="hideConfirm"
+             ref="confirm"></confirm>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import Confirm from '../../components/base/confirm/confirm.vue'
+  export default{
+    data() {
+      return {
+        text: '我将要被删除'
+      }
+    },
+    components: {
+      Confirm
+    },
+    methods: {
+      showConfirm() {
+        this.$refs.confirm.show();
+      },
+      hideConfirm() {
+        this.$refs.confirm.hide();
+      },
+      confirmClear() {
+        this.text = '';
+        this.$refs.confirm.hide();
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
