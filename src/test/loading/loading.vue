4737a4ed592650cb7e58a643714190df3fc99226
lml19960131 <lml19960131@126.com>
2018-02-18 11:16:13 +0800

    loading-test

---
+++ src/test/loading/loading.vue
@@ -0,0 +1,16 @@
+<template>
+  <loading showFlag="true" title="正在加载"></loading>
+</template>
+
+<script type="text/ecmascript-6">
+  import Loading from '../../components/base/loading/loading.vue'
+  export default{
+    components: {
+      Loading
+    }
+  }
+</script>
+
+<style scoped lang='stylus' rel="stylesheet/stylus">
+
+</style>
