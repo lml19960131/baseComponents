b16ef902d22ce5434004a70ea3ccc7c2ed48d2ad
lml19960131 <lml19960131@126.com>
2018-02-18 13:27:06 +0800

    topTip-test

---
+++ src/test/topTip/topTip.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @click="showTopTip">click me</button>
+    <top-tip ref="topTip" :showFlag="false" delayTime="1000">
+      <span>命令被执行</span>
+    </top-tip>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+ import TopTip from '../../components/base/topTip/topTip.vue'
+
+  export default{
+    components: {
+      TopTip
+    },
+    methods:{
+      showTopTip() {
+        this.$refs.topTip.show();
+      }
+    }
+  }
+</script>
+
+<style scoped lang='stylus' rel="stylesheet/stylus">
+
+</style>
